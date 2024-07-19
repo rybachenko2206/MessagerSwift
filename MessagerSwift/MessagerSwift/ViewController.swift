cddea7d4bc2681478b17d50a1b273929f82c6bf5
Roman Rybachenko <rom_rybachenko@ukr.net>
2024-07-19 14:14:08 +0200

    Initial commit

---
+++ MessagerSwift/MessagerSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessagerSwift
+//
+//  Created by Roman Rybachenko on 19.07.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
